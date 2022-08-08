293dd886579706079e6489d12554880872e2c43f
Akshay2251996 <110330307+Akshay2251996@users.noreply.github.com>
2022-08-08 14:13:06 +0530

    Add files via upload

---
+++ practice data excel,csv&tsv.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+pd.read_excel(r"C:\Users\user\Desktop\classdata1.xlsx")
+
+
+# In[8]:
+
+
+pd.read_csv(r'C:\Users\user\Desktop\classdata1.1.csv')
+
+
+# In[12]:
+
+
+data=pd.read_csv(r'C:\Users\user\Desktop\classdata.tsv')
+
+
+# In[13]:
+
+
+data
+
+
+# In[ ]:
+
+
+
+
