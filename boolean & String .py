23d602423daba15fe0fd049bf6b91645bd8e14ac
Akshay2251996 <110330307+Akshay2251996@users.noreply.github.com>
2022-08-08 13:19:52 +0530

    Add files via upload

---
+++ boolean & String .py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+True and False
+
+
+# In[3]:
+
+
+True and True
+
+
+# In[4]:
+
+
+True or False
+
+
+# In[5]:
+
+
+False and True
+
+
+# In[6]:
+
+
+False and False
+
+
+# In[7]:
+
+
+False or True
+
+
+# In[8]:
+
+
+True and False or True
+
+
+# In[9]:
+
+
+False and True or False
+
+
+# In[10]:
+
+
+False or False
+
+
+# In[15]:
+
+
+True + True
+
+
+# In[16]:
+
+
+x=1
+type(x)
+
+
+# In[17]:
+
+
+x=2.0
+type(x)
+
+
+# In[18]:
+
+
+x1=True
+x2=False
+type(x1)
+
+
+# In[19]:
+
+
+name='xyz'
+type(name)
+
+
+# In[20]:
+
+
+ten=10
+ten
+
+
+# In[21]:
+
+
+ten=ten
+
+
+# In[22]:
+
+
+ten
+
+
+# In[23]:
+
+
+ten='ten'
+ten
+
+
+# In[27]:
+
+
+'day'+ str(1)
+
+
+# In[28]:
+
+
+a=20
+print(a,"is a",type(a),type)
+
+
+# In[29]:
+
+
+b=20.1
+print(b,"is b",type(b),type)
+
+
+# In[33]:
+
+
+a='love'
+b='is'
+c='python'
+print(a+b+c)
+
+
+# In[34]:
+
+
+a=[20,'love',25.5]
+print(a)
+
+
+# In[35]:
+
+
+b=(20,'love',25.5)
+print(b)
+
+
+# In[37]:
+
+
+c={100,'love',1,"set",1,"set",100,1}
+print(c)
+
+
+# In[38]:
+
+
+False + True
+
+
+# In[ ]:
+
+
+
+
---
+++ excel,csv,tsv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[12]:
+
+
+pd.read_excel(r"C:\Users\user\Desktop\Book1.xlsx")
+
+
+# In[19]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\book 2.csv")
+
+
+# In[21]:
+
+
+pd.read_csv(r'C:\Users\user\Desktop\data1.tsv')
+
+
+# In[ ]:
+
+
+
+
